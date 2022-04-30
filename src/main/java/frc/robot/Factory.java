d9c4301d55732e8927c69eb294da8858257d92f7
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2022-04-29 22:58:06 -0700

    Few code and gradle updates

--- src/main/java/frc/robot/Factory.java
+++ src/main/java/frc/robot/Factory.java
@@ -8,7 +8,7 @@ import frc.robot.subsystems.drivebase.ArcadeDrive;
 import frc.robot.subsystems.drivebase.DriveBaseSub;
 import frc.robot.subsystems.intake.IntakeSub;
 import frc.robot.subsystems.intake.RunIntake;
-import frc.robot.subsystems.intake.RunIntakeWithJoystick;
+// import frc.robot.subsystems.intake.RunIntakeWithJoystick;
 import frc.robot.subsystems.drivebase.TankDrive;
 
 public interface Factory {
@@ -16,7 +16,7 @@ public interface Factory {
     public IntakeSub getIntakeSub();
     public PaddedXbox getPaddedXbox();
     public RunIntake getRunIntakeWithPower(double power);
-    public RunIntakeWithJoystick getRunIntakeWithJoystick(PaddedXbox joystick);
+    // public RunIntakeWithJoystick getRunIntakeWithJoystick(PaddedXbox joystick);
     public DriveBaseSub getDriveBaseSub();
     public TankDrive getTankDrive(PaddedXbox joystick);
     public ArcadeDrive getArcadeDrive(PaddedXbox joystick);
